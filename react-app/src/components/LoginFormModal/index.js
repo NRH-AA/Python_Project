a8922d5b286180b41d00f0bd0eb2457bf881100d
John <tim@cruz-family.ws>
2023-03-12 19:36:18 -0700

    login/sign up functional

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -4,17 +4,25 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import "./LoginForm.css";
 
-function EmailFormModal() {
+function LoginFormModal() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
+  const [isEmailEntered, setIsEmailEntered] = useState(false);
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("")
   const [errors, setErrors] = useState([]);
-  const [isEmailEntered, setIsEmailEntered] = useState(false)
-  const [isEmailInUse, setIsEmailInUse] = useState(true)
   const { closeModal } = useModal();
 
-  const handleSubmit = async (e) => {
+  const backtrack = () => {
+    isEmailEntered ? setIsEmailEntered(false) : closeModal();
+    setErrors([]);
+  };
+
+  const handleEmailSubmit = async (e) => {
+    e.preventDefault();
+    setIsEmailEntered(true);
+  };
+
+  const handleLoginSubmit = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
     if (data) {
@@ -24,98 +32,63 @@ function EmailFormModal() {
     }
   };
 
-  const signInOrLogin = () => {
-    setIsEmailEntered(true)
-    // In here needs to be logic to see if the email
-    // provided is in use already.
-  }
-
   const signInDemoUser = () => {
     return dispatch(login('demo@aa.io', 'password'))
-      .then(closeModal)
-  }
+      .then(closeModal);
+  };
 
   return (
     <>
-      <i id='backtrack-button' className="fa-solid fa-arrow-left" onClick={isEmailEntered ? (e) => setIsEmailEntered(false) : (e) => closeModal()} />
-      <div id='login-container'>
+      <i id='backtrack-button' className="fa-solid fa-arrow-left" onClick={backtrack} />
+      <div id='login-modal-container'>
         <h1 id="login-title">scrollr</h1>
-        <form noValidate id='login-form' onSubmit={handleSubmit}>
-          {isEmailEntered === false ?
-            <>
-              <h4 id='login-prompt'>Enter your email to log in or register:</h4>
-              <label className="input-label">
-                <input
-                  className="input-field"
-                  type="text"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </label>
-              <button className='form-button' type='button' disabled={email === ''} onClick={(e) => signInOrLogin()}>
-                Next
-                <i className="fa-solid fa-arrow-right form-arrow-img" />
-              </button>
-            </>
-            :
-            false ?
-            // isEmailInUse ?
-              <>
-                <h4>Welcome back to your corner of the internet.</h4>
-                <h4>Glad you're here.</h4>
-                <label className="input-label">
-                  <input
-                    className="input-field"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </label>
-                <button className="form-button" type="submit">
-                  Log in
-                  <i className="fa-solid fa-arrow-right form-arrow-img" />
-                </button>
-              </>
-              :
-              <>
-                <h4>Welcome back to your corner of the internet.</h4>
-                <h4>Glad you're here.</h4>
-                <input
-                  className="input-field"
-                  type="password"
-                  placeholder="Set a password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <input
-                  className="input-field"
-                  type="password"
-                  placeholder="Repeat password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required
-                />
-                <button className='form-button' type='button'>
-                  Next
-                  <i className="fa-solid fa-arrow-right form-arrow-img" />
-                </button>
-              </>
-          }
-          <ul>
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul>
+        <form noValidate className={isEmailEntered ? 'hidden login-form' : 'login-form'} onSubmit={handleEmailSubmit}>
+          <h4>Enter your email to log in or register:</h4>
+          <label className="login-input-label">
+            <input
+              className="login-input-field"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          <button className='login-form-button' type='submit' disabled={email === ''}>
+            Next
+            <i className="fa-solid fa-arrow-right login-form-arrow-img" />
+          </button>
         </form>
+
+        <form noValidate className={isEmailEntered ? 'login-form' : 'hidden login-form'} onSubmit={handleLoginSubmit}>
+          <h4>Welcome back to your corner of the internet.</h4>
+          <h4>Glad you're here.</h4>
+          <label className="login-input-label">
+            <input
+              className="login-input-field"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button className="login-form-button" type="submit">
+            Log in
+            <i className="fa-solid fa-arrow-right login-form-arrow-img" />
+          </button>
+        </form>
+        
+        <ul id='login-form-errors'>
+          {errors.map((error, idx) => (
+            <li className='login-form-error' key={idx}>{error}</li>
+          ))}
+        </ul>
+
         <p id='demo-user-sign-in' onClick={signInDemoUser}>demo user</p>
       </div>
     </>
   );
-}
+};
 
-export default EmailFormModal;
+export default LoginFormModal;
--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -7,75 +7,150 @@ import "./SignupForm.css";
 function SignupFormModal() {
 	const dispatch = useDispatch();
 	const [email, setEmail] = useState("");
-	const [username, setUsername] = useState("");
+	const [isEmailEntered, setIsEmailEntered] = useState(false);
 	const [password, setPassword] = useState("");
+	const [isPasswordEntered, setIsPasswordEntered] = useState(false);
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [isFirstAndLastNameEntered, setIsFirstAndLastNameEntered] = useState(false);
 	const [confirmPassword, setConfirmPassword] = useState("");
+	const [username, setUsername] = useState("");
 	const [errors, setErrors] = useState([]);
 	const { closeModal } = useModal();
 
-	const handleSubmit = async (e) => {
+	const backtrack = () => {
+		isEmailEntered ? setIsEmailEntered(false) : closeModal();
+		setErrors([]);
+	}
+
+	const handleEmailSubmit = async (e) => {
+		e.preventDefault();
+		setIsEmailEntered(true);
+	};
+
+	const handlePasswordSubmit = async (e) => {
 		e.preventDefault();
 		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, email, password));
-			if (data) {
-				setErrors(data);
-			} else {
-				closeModal();
-			}
+			setIsPasswordEntered(true);
 		} else {
-			setErrors([
-				"Confirm Password field must be the same as the Password field",
-			]);
+			setErrors(['password: Your passwords should match'])
+		}
+	};
+
+	const handleFirstAndLastNameSubmit = async (e) => {
+		e.preventDefault();
+		setIsFirstAndLastNameEntered(true);
+	}
+
+	const handleSignUpSubmit = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(signUp(username, email, password, firstName, lastName));
+		if (data) {
+			setErrors(data);
+		} else {
+			closeModal();
 		}
 	};
 
 	return (
 		<>
-			<h1>Sign Up</h1>
-			<form onSubmit={handleSubmit}>
-				<ul>
-					{errors.map((error, idx) => (
-						<li key={idx}>{error}</li>
-					))}
-				</ul>
-				<label>
-					Email
+			<i id='backtrack-button' className="fa-solid fa-arrow-left" onClick={backtrack} />
+			<div id='sign-up-modal-container'>
+				<h1 id="sign-up-title">scrollr</h1>
+				<form noValidate className={isEmailEntered ? 'hidden sign-up-form' : 'sign-up-form'} onSubmit={handleEmailSubmit}>
+					<h4>Enter your email to log in or register:</h4>
+					<label className="sign-up-input-label">
+						<input
+							className="sign-up-input-field"
+							type="text"
+							placeholder="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</label>
+					<button className='sign-up-form-button' type='submit' disabled={email === ''}>
+						Next
+						<i className="fa-solid fa-arrow-right sign-up-form-arrow-img" />
+					</button>
+				</form>
+
+				<form noValidate className={isEmailEntered && (!isPasswordEntered) ? 'sign-up-form' : 'hidden sign-up-form'} onSubmit={handlePasswordSubmit}>
+					<h4>Welcome back to your corner of the internet.</h4>
+					<h4>Glad you're here.</h4>
+					<label className="sign-up-input-label">
+						<input
+							className="sign-up-input-field"
+							type="password"
+							placeholder="Set a password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</label>
+					<label className="sign-up-input-label">
+						<input
+							className="sign-up-input-field"
+							type="password"
+							placeholder="Repeat password"
+							value={confirmPassword}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+							required
+						/>
+					</label>
+					<button className='sign-up-form-button' type='submit' disabled={password === '' || confirmPassword === ''}>
+						Next
+						<i className="fa-solid fa-arrow-right sign-up-form-arrow-img" />
+					</button>
+				</form>
+
+				<form noValidate className={isPasswordEntered && (!isFirstAndLastNameEntered) ? 'sign-up-form' : 'hidden sign-up-form'} onSubmit={handleFirstAndLastNameSubmit}>
 					<input
+						className="sign-up-input-field"
 						type="text"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
+						placeholder="First name"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
 						required
 					/>
-				</label>
-				<label>
-					Username
 					<input
+						className="sign-up-input-field"
 						type="text"
+						placeholder="Last name"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+						required
+					/>
+					<button className='sign-up-form-button' type='submit' disabled={firstName === '' || lastName === ''}>
+						Next
+						<i className="fa-solid fa-arrow-right sign-up-form-arrow-img" />
+					</button>
+				</form>
+
+				<form noValidate className={isFirstAndLastNameEntered ? 'sign-up-form' : 'hidden sign-up-form'} onSubmit={handleSignUpSubmit}>
+					<h4>What should we call you?</h4>
+					<h4>This will be how you appear to others on Scrollr, and your URL. Don't worry, you can change this later.</h4>
+					<input
+						className="sign-up-input-field"
+						type="text"
+						placeholder="Blog name"
 						value={username}
 						onChange={(e) => setUsername(e.target.value)}
 						required
 					/>
-				</label>
-				<label>
-					Password
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Confirm Password
-					<input
-						type="password"
-						value={confirmPassword}
-						onChange={(e) => setConfirmPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<button type="submit">Sign Up</button>
-			</form>
+					<button className='sign-up-form-button' type='submit' disabled={username === ''}>
+						Sign up
+						<i className="fa-solid fa-arrow-right sign-up-form-arrow-img" />
+					</button>
+				</form>
+
+				<ul id='sign-up-form-errors'>
+					{errors.map((error, idx) => (
+						<li className='sign-up-form-error' key={idx}>{error}</li>
+					))}
+				</ul>
+
+			</div>
 		</>
 	);
 }
