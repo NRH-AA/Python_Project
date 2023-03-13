3066bb23f0dfe52b930ad1894ba0ce08186fd2e9
John <tim@cruz-family.ws>
2023-03-12 17:56:12 -0700

    unfinished login modal

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -4,11 +4,14 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import "./LoginForm.css";
 
-function LoginFormModal() {
+function EmailFormModal() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("")
   const [errors, setErrors] = useState([]);
+  const [isEmailEntered, setIsEmailEntered] = useState(false)
+  const [isEmailInUse, setIsEmailInUse] = useState(true)
   const { closeModal } = useModal();
 
   const handleSubmit = async (e) => {
@@ -17,41 +20,102 @@ function LoginFormModal() {
     if (data) {
       setErrors(data);
     } else {
-        closeModal()
+      closeModal()
     }
   };
 
+  const signInOrLogin = () => {
+    setIsEmailEntered(true)
+    // In here needs to be logic to see if the email
+    // provided is in use already.
+  }
+
+  const signInDemoUser = () => {
+    return dispatch(login('demo@aa.io', 'password'))
+      .then(closeModal)
+  }
+
   return (
     <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
+      <i id='backtrack-button' className="fa-solid fa-arrow-left" onClick={isEmailEntered ? (e) => setIsEmailEntered(false) : (e) => closeModal()} />
+      <div id='login-container'>
+        <h1 id="login-title">scrollr</h1>
+        <form noValidate id='login-form' onSubmit={handleSubmit}>
+          {isEmailEntered === false ?
+            <>
+              <h4 id='login-prompt'>Enter your email to log in or register:</h4>
+              <label className="input-label">
+                <input
+                  className="input-field"
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              <button className='form-button' type='button' disabled={email === ''} onClick={(e) => signInOrLogin()}>
+                Next
+                <i className="fa-solid fa-arrow-right form-arrow-img" />
+              </button>
+            </>
+            :
+            false ?
+            // isEmailInUse ?
+              <>
+                <h4>Welcome back to your corner of the internet.</h4>
+                <h4>Glad you're here.</h4>
+                <label className="input-label">
+                  <input
+                    className="input-field"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </label>
+                <button className="form-button" type="submit">
+                  Log in
+                  <i className="fa-solid fa-arrow-right form-arrow-img" />
+                </button>
+              </>
+              :
+              <>
+                <h4>Welcome back to your corner of the internet.</h4>
+                <h4>Glad you're here.</h4>
+                <input
+                  className="input-field"
+                  type="password"
+                  placeholder="Set a password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <input
+                  className="input-field"
+                  type="password"
+                  placeholder="Repeat password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+                <button className='form-button' type='button'>
+                  Next
+                  <i className="fa-solid fa-arrow-right form-arrow-img" />
+                </button>
+              </>
+          }
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+        </form>
+        <p id='demo-user-sign-in' onClick={signInDemoUser}>demo user</p>
+      </div>
     </>
   );
 }
 
-export default LoginFormModal;
+export default EmailFormModal;
