4b1f6891bcdf1c0d6057f7f80bc9424598c434c1
John <tim@cruz-family.ws>
2023-03-13 10:20:35 -0700

    refactored posts.js

--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -6,88 +6,95 @@ const READ_POSTS = "/posts"
 const READ_POST = "/posts/:postId"
 
 
+export const createPosts = (post) => ({
+    type: CREATE_POST,
+    payload: post
+})
 
-// export const createPost = (post, userId) => async (dispatch) => {
-//     const {user_id, post_title, post_heading, post_text, imageUrl} = post
-//     const data = await fetch(`/api/${userId}/posts`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//             user_id,
-//             post_title,
-//             post_heading,
-//             post_text,
-//             imageUrl
-//         })
-//     })
-//     if (data.ok) {
-//     const response = await data.Json()
-//     dispatch(createPosts(response))
-//     return response
-
-//     } else if (response.status < 500) {
-// 		const data = await response.json();
-// 		if (data.errors) {
-// 			return data.errors;
-// 		}
-// 	} else {
-// 		return ["An error occurred. Please try again."];
-// 	}
-// }
-
-// export const createPosts = (post) =>({
-//     type: CREATE_POST,
-//     payload: post
-// })
-
-// export const deletePost = (id) => async (dispatch) => {
-//     const response = await fetch(`/api/posts/${id}`, {
-//         method: "DELETE",
-//     })
-//     dispatch(deletePosts(id));
-//     return response
-// }
-
-// export const deletePosts = (id) => {
-//     return{
-//         type:DELETE_POST,
-//         id
-//     }
-// }
-
-// export const updatePost = (id) => async (dispatch) => {
-//    const {user_id, post_title, post_heading, post_text} = post
-//     const data = await fetch(`/api/posts/${id}`, {
-//         method: "PUT",
-//         body: JSON.stringify({
-//             user_id,
-//             post_title,
-//             post_heading,
-//             post_text
-
-//         }),
-//     });
-//     if (data.ok) {
-//         const response = await data.Json()
-//         dispatch(updatePosts(response))
-//         return response
-//         } else if (response.status < 500) {
-//             const data = await response.json();
-//             if (data.errors) {
-//                 return data.errors;
-//             }
-//         } else {
-//             return ["An error occurred. Please try again."];
-//         }
-//     }
+export const createPost = (post, userId) => async (dispatch) => {
+    const { user_id, post_title, post_heading, post_text, imageUrl } = post
+    const data = await fetch(`/api/${userId}/posts`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            user_id,
+            post_title,
+            post_heading,
+            post_text,
+            imageUrl
+        })
+    })
+    if (data.ok) {
+        const response = await data.json()
+        dispatch(createPosts(response))
+        return response
+    }
+    return data;
+    // else if (response.status < 500) {
+    //     const data = await response.json();
+    //     if (data.errors) {
+    //         return data.errors;
+    //     }
+    // } else {
+    //     return ["An error occurred. Please try again."];
+    // }
+}
 
 
-// export const updatePosts = (post) =>({
-//     type: UPDATE_POST,
-//     payload: post
-// })
+export const deletePosts = (id) => {
+    return {
+        type: DELETE_POST,
+        id
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    const response = await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+    })
+    dispatch(deletePosts(id));
+    return response
+}
+
+
+export const updatePosts = (post) => ({
+    type: UPDATE_POST,
+    payload: post
+})
+
+export const updatePost = (id, post) => async (dispatch) => {
+    const { user_id, post_title, post_heading, post_text } = post
+    const data = await fetch(`/api/posts/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+            user_id,
+            post_title,
+            post_heading,
+            post_text
+
+        }),
+    });
+    if (data.ok) {
+        const response = await data.json();
+        dispatch(updatePosts(response));
+        return response;
+    };
+    return data;
+    // else if (response.status < 500) {
+    //     const data = await response.json();
+    //     if (data.errors) {
+    //         return data.errors;
+    //     }
+    // } else {
+    //     return ["An error occurred. Please try again."];
+    // }
+}
+
+
+
+
 export const readPosts = (posts) => ({
     type: READ_POSTS,
     payload: posts
@@ -111,21 +118,21 @@ export const getPosts = () => async (dispatch) => {
 }
 
 
-
-// export const getUser = (userId) => async (dispatch) => {
-//     const data = await fetch(`/api`)
-
-//     const posts = await data.json()
-
-//     dispatch(readPost(posts))
-//     return posts
-// }
-
 export const readPost = (post) => ({
     type: READ_POST,
     payload: post
 })
 
+export const getUser = (userId) => async (dispatch) => {
+    const data = await fetch(`/api`)
+
+    if (data.ok) {
+        const posts = await data.json()
+        dispatch(readPost(posts))
+        return posts
+    }
+    return data
+}
 
 
 const initialState = { allPosts: {}, singlePost: {} }
@@ -154,5 +161,4 @@ export default function postReducer(state = initialState, action) {
         default:
             return state;
     };
-
 }
