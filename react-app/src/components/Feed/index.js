4ed61b70f2e1ced4e8cc7aadf83d52e02fc2e231
John <tim@cruz-family.ws>
2023-03-14 10:58:39 -0700

    working on comments

--- react-app/src/components/Feed/index.js
+++ react-app/src/components/Feed/index.js
@@ -1,42 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../store/posts"
+import SinglePost from "../SinglePost";
 import "./Feed.css";
 
 function Feed() {
     const dispatch = useDispatch();
 
     const posts = useSelector(state => state.posts.allPosts)
-
-    // const commentPlaceholders = [
-    //     "Add to the discussion",
-    //     "Say your thing",
-    //     "Add something wonderful",
-    //     "Your words here",
-    //     "Have something to say?",
-    //     "Unleash a compliment",
-    //     "Reply your heart out",
-    //     "Send something nice"
-    // ];
+    const session = useSelector(state => state.session)
 
     useEffect(() => {
         dispatch(getPosts())
     }, [dispatch])
-
-    const handleCommentButton = async (e) => {
-        console.log('Comment button is working!', e.target)
-    };
-
-    const handleLikeButton = async (e) => {
-        console.log('Like button is working!', e.target.classList)
-    };
-
-    // const handleCommentSubmit = async (e) => {
-    //     e.preventDefault();
-    //     console.log('Submit comment button is working!')
-    //     console.log('Here is the comment being sent: ', e.target.children[0].value)
-    //     console.log('Here is the post id: ', e.target.classList[0])
-    // };
 
     return (
         <div id='dashboard'>
@@ -53,70 +29,8 @@ function Feed() {
                             </div>
                             <h2 className="post-title">{post.post_title}</h2>
                             <img className={post.imageURL !== null ? "post-image" : "hidden"} src={post.imageURL} alt=''></img>
-                            <div className={post.post_heading ? "post-heading" : "hidden"}>{post.post_heading}</div>
                             <div className={post.post_text ? "post-text" : "hidden"}>{post.post_text}</div>
-                            <div className="post-option-container">
-                                <div className="post-options">
-                                    <div className="view-comments-button">
-                                        {post.comments.length} notes
-                                    </div>
-                                    <div className="comment-and-like-button-container">
-                                        <i className={`fa-sharp fa-regular fa-comment fa-xl comment-button ${post.id}`} onClick={(e) => handleCommentButton(e)} />
-                                        <i className={`fa-regular fa-heart fa-xl like-button ${post.id}`} onClick={(e) => handleLikeButton(e)} />
-                                        <i className={`fa-solid fa-heart fa-xl hidden ${post.id}`} onClick={(e) => handleLikeButton(e)} />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className={post.comments.length ? "post-comments-container" : "hidden"}>
-                                <div className="post-stats-container">
-                                    <div className="post-stats">
-                                        <div className="post-comment-count-container">
-                                            <i className="fa-sharp fa-regular fa-comment fa-lg comment-button" />
-                                            <div className="post-comment-count">{post.comments.length}</div>
-                                        </div>
-                                        <div className="post-like-count-container">
-                                            <i className="fa-sharp fa-regular fa-heart fa-lg like-button" />
-                                            <div className="post-like-count">{post.likes}</div>
-                                        </div>
-                                    </div>
-                                    <div className="comment-order-selector-container">
-                                        {/* <div className="comment-order-selector">
-
-                                        </div> */}
-                                    </div>
-                                </div>
-                                {/* <div className="make-comment-container">
-                                    <div className="current-user-image">
-
-                                    </div>
-                                    <form className="type-comment-box-container" onSubmit={(e) => handleCommentSubmit(e)}>
-                                        <input className={`${post.id} type-comment-box`}
-                                            type="text"
-                                            placeholder={commentPlaceholders[Math.floor(Math.random() * commentPlaceholders.length)]}
-                                            required
-                                        />
-                                        <button className={`${post.id} submit-comment-button`}
-                                            type="submit"
-                                        >
-                                            Reply
-                                        </button>
-                                    </form>
-                                </div> */}
-                                {post.comments.map((comment, idx) => (
-                                    <div className="post-comment-container" key={idx}>
-                                        <img className="post-commenter-image" src={comment.user.profile_picture} alt="post commenter"></img>
-                                        <div className="post-comment-box">
-                                            <div className="post-commenter-username">
-                                                {comment.user.username}
-                                            </div>
-                                            <div className="post-comment" key={idx}>
-                                                {comment.comment}
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
+                            <SinglePost info={[post, session]} />
                         </div>
                     </div>
                 )
---
+++ react-app/src/components/SinglePost/index.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import "./SinglePost.css";
+
+function SinglePost({ info }) {
+    const [openComments, setOpenComments] = useState(false);
+    const [likePost, setLikePost] = useState(false);
+    const [viewStat, setViewStat] = useState("comments");
+    const [comment, setComment] = useState("")
+    const [post, session] = info
+
+    const commentPlaceholders = [
+        "Add to the discussion",
+        "Say your thing",
+        "Add something wonderful",
+        "Your words here",
+        "Have something to say?",
+        "Unleash a compliment",
+        "Reply your heart out",
+        "Send something nice"
+    ];
+
+    const handleLikeButton = () => {
+        setLikePost(!likePost);
+    };
+
+    const handleCommentSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Comment submit button is working! Input: ", comment)
+    };
+
+    return (
+        <div className="post-comment-section-container">
+            <div className="post-option-container">
+                <div className="post-options">
+                    <div className={`view-comments-button ${openComments ? "hidden" : "show"}`} onClick={() => setOpenComments(!openComments)}>
+                        {post.comments.length} notes
+                    </div>
+                    <div className={`close-comments-button ${openComments ? "show" : "hidden"}`} onClick={() => setOpenComments(!openComments)}>
+                        <i className="fa-solid fa-x fa-xs close-comments-button-image" />
+                        <p className="close-notes-button-text">Close notes</p>
+                    </div>
+                    <div className="comment-and-like-button-container">
+                        <i className={"fa-sharp fa-regular fa-comment fa-xl comment-button"} onClick={() => setOpenComments(!openComments)} />
+                        <i className={`fa-heart fa-xl like-button ${likePost ? "liked fa-solid" : "fa-regular"}`} onClick={handleLikeButton} />
+                    </div>
+                </div>
+            </div>
+
+            <div className={openComments ? "post-comments-container" : "hidden"}>
+                <div className="post-stats-container">
+                    <div className="post-stats">
+                        <div className={`post-comment-count-container ${viewStat === "comments" && "viewing"}`} onClick={() => setViewStat('comments')}>
+                            <i className="fa-sharp fa-regular fa-comment fa-lg comment-button" />
+                            <div className="post-comment-count">{post.comments.length}</div>
+                        </div>
+                        <div className={`post-like-count-container ${viewStat === "likes" && "viewing"}`} onClick={() => setViewStat('likes')}>
+                            <i className="fa-sharp fa-regular fa-heart fa-lg like-button" />
+                            <div className="post-like-count">{post.likes}</div>
+                        </div>
+                    </div>
+                    <div className="comment-order-selector-container">
+                        {/* <div className="comment-order-selector">
+
+                        </div> */}
+                    </div>
+                </div>
+                <div className={`make-comment-container ${!session.user && "hidden"}`}>
+                    <div className="current-user-image-container">
+                        <img className="current-user-image" src={session.user.profile_picture} alt="user profile"></img>
+                    </div>
+                    <form className="type-comment-box-container" onSubmit={(e) => handleCommentSubmit(e)}>
+                        <textarea className="type-comment-box"
+                            rows="1"
+                            type="text"
+                            placeholder={commentPlaceholders[Math.floor(Math.random() * commentPlaceholders.length)]}
+                            onChange={(e) => setComment(e.target.value)}
+                        />
+                        <button className="submit-comment-button"
+                            type="submit"
+                            disabled={comment === ""}
+                        >
+                            Reply
+                        </button>
+                    </form>
+                </div>
+                {post.comments.length ? post.comments.map((comment, idx) => (
+                    <div className="post-comment-container" key={idx}>
+                        <img className="post-commenter-image" src={comment.user.profile_picture} alt="post commenter"></img>
+                        <div className="post-comment-box">
+                            <div className="post-commenter-username">
+                                {comment.user.username}
+                            </div>
+                            <div className="post-comment" key={idx}>
+                                {comment.comment}
+                            </div>
+                        </div>
+                    </div>
+                ))
+            :
+            <p className="hidden"></p>
+            }
+            </div>
+        </div>
+    )
+}
+
+export default SinglePost
