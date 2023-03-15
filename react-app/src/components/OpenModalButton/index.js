77c80b3d502557655e7ca818d62f93d7fa048420
NRH-AA <nheinz1995@live.com>
2023-03-14 17:26:11 -0700

    Allow OpenModalButton to be an image

--- react-app/src/components/OpenModalButton/index.js
+++ react-app/src/components/OpenModalButton/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { useModal } from '../../context/Modal';
 
+function buttonElement(buttonText, onClick) {
+    return (<button onClick={onClick}>{buttonText}</button>)
+}
+
+function imageElement(img, onClick) {
+  return (<img 
+            src={img} 
+            onClick={onClick}
+         />)
+}
+
 function OpenModalButton({
   modalComponent, // component to render inside the modal
   buttonText, // text of the button that opens the modal
   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
+  onModalClose, // optional: callback function that will be called once the modal is closed
+  img
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
@@ -15,9 +27,11 @@ function OpenModalButton({
     if (onButtonClick) onButtonClick();
   };
 
-  return (
-    <button onClick={onClick}>{buttonText}</button>
-  );
+  if (img) {
+    return imageElement(img, onClick)
+  } else {
+    return buttonElement(buttonText, onClick);
+  }
 }
 
 export default OpenModalButton;
