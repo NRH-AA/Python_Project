70e19eb0f2f759d371e464cbdd6f4afc8b2c69f0
NRH-AA <nheinz1995@live.com>
2023-03-15 19:26:54 -0700

    Add image upload functionality

--- react-app/src/components/CreatePosts/index.js
+++ react-app/src/components/CreatePosts/index.js
@@ -13,71 +13,108 @@ function CreatePostForm({ id }) {
   //   const posts = useSelector(state=>state.posts.allPosts)
   const user = useSelector(state => state.session.user)
   const [post_title, setPostTitle] = useState("");
-  const [post_heading, setPostHeading] = useState("");
   const [post_text, setPostText] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [image, setImage] = useState("");
+  const [imageLoading, setImageLoading] = useState(false);
+  const [imageURL, setImageURL] = useState('')
 
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
   const history = useHistory()
 
-
-
-  const handleSubmit = (e) => {
+  
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     setErrors([]);
     if (!user) return setErrors(["You Must Be Logged in To Create A post"])
-    dispatch(postsActions.createPost({ post_title, post_text }, user.id))
+      
+    dispatch(postsActions.createPost({ post_title, imageURL, post_text }, user.id))
       .then(closeModal)
       .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors);
+            history.push(`/posts`);
       });
-
-    history.push(`/posts`)
-
+  };
+  
+  const handleImageUpload = async () => {
+      const formData = new FormData();
+      formData.append("image", image);
+      
+      setImageLoading(true);
+      const res = await fetch('/api/users/upload', {
+          method: "POST",
+          body: formData,
+      });
+      
+      if (res.ok) {
+        const data = await res.json();
+        const imageUrl = data.url
+        console.log("ImageURL", imageUrl)
+        
+        if (!imageUrl) return setErrors(["Failed to upload image. Please try again."])
+        setImageURL(imageUrl)
+        setImageLoading(false);
+      }
+  }
+  
+  const updateImage = (e) => {
+      const file = e.target.files[0];
+      setImage(file);
   }
 
   return (
     <div id="createForm">
-
-
       <h4 className="create-form-text" >{user.username}</h4>
+      
       <form onSubmit={handleSubmit} autoComplete="on">
         <ul>
           {errors.map((error, idx) => <li key={idx}>{error}</li>)}
         </ul>
+        
         <label className="create-input-label">
-
           <input className="input"
-            placeholder="Title"
+            placeholder="Post Title"
             type="text"
             id="PostTitle"
-
             value={post_title}
             onChange={(e) => setPostTitle(e.target.value)}
             required
           />
         </label>
+        
+        <input
+              type="file"
+              accept="image/*"
+              onChange={updateImage}
+            />
+          <button
+            onClick={() => handleImageUpload()}
+            type="button"
+          >Submit</button>
+          {(imageLoading)&& <p>Loading...</p>}
 
         <label className="labels">
-
-          <textarea className="create-post-text" placeholder="text"
-            maxLength="500"
+          <textarea className="create-post-text" 
+            placeholder="What would you like to say?"
+            maxLength="250"
             cols="20"
-
             rows="10"
             value={post_text}
             onChange={(e) => setPostText(e.target.value)}
             required>
           </textarea>
-
         </label>
 
         <div className="button">
-
-          <button className='create-form-button' type="submit" >Create Posts</button>
+          <button 
+            className='create-form-button' 
+            type="submit" 
+            disabled={imageLoading ? "true" : ""}
+          >Create Posts</button>
         </div>
+        
       </form>
     </div>
   );
