29871533e1ff3cf66682dd43a07585443d51ea7b
NRH-AA <nheinz1995@live.com>
2023-03-17 11:36:34 -0700

    Signup form modal fixes

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { signUp } from "../../store/session";
+import { isValidEmail } from "../../store/user";
 import "./SignupForm.css";
 
 function SignupFormModal() {
@@ -19,29 +20,51 @@ function SignupFormModal() {
 		isEmailEntered ? setIsEmailEntered(false) : closeModal();
 		setErrors([]);
 	}
+	
+	const checkValidEmail = () => {
+		if (!email.includes('@') || !email.includes('.')) return ["Email is invalid"];
+		if (!email || !email.length > 6) return ["Email must be 6 characters or more"];
+		
+		const textAfterPeriod = email.split(".")[1]
+		const textAfterAmpersand = email.split("@")[1]
+		
+		if (!textAfterPeriod || !textAfterAmpersand) return ["Email is invalid"]
+		return false
+	}
 
 	const handleEmailSubmit = async (e) => {
 		e.preventDefault();
-		setIsEmailEntered(true);
+		
+		const checkEmail = checkValidEmail();
+		if (checkEmail) return setErrors(checkEmail)
+		
+		setErrors([]);
+		dispatch(isValidEmail(email))
+		.then(async (res) => {
+		  if (res && !res.errors) return setErrors(['Email is taken.']);
+		  setIsEmailEntered(true);
+		});
 	};
 
 	const handlePasswordSubmit = async (e) => {
 		e.preventDefault();
-		if (password === confirmPassword) {
-			setIsPasswordEntered(true);
-		} else {
-			setErrors(['password: Your passwords should match'])
-		}
+		
+		if (!password || password.length < 6) return setErrors(['Password must be 6 characters or more']);
+		
+		if (password !== confirmPassword) return setErrors(['password: Your passwords should match'])
+		
+		setIsPasswordEntered(true);
 	};
 
 	const handleSignUpSubmit = async (e) => {
 		e.preventDefault();
+		
+		if (!username || username.length < 4) return setErrors(['Username must be 4 characters or more']);
+		
 		const data = await dispatch(signUp(username, email, password));
-		if (data) {
-			setErrors(data);
-		} else {
-			closeModal();
-		}
+		if (data) return setErrors(data);
+
+		closeModal();
 	};
 
 	return (
@@ -54,7 +77,7 @@ function SignupFormModal() {
 					<label className="sign-up-input-label">
 						<input
 							className="sign-up-input-field"
-							type="text"
+							type="email"
 							placeholder="Email"
 							value={email}
 							onChange={(e) => setEmail(e.target.value)}
