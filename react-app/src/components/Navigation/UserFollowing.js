f0969a8d6e3674defe785763cdfea08281b51591
John <tim@cruz-family.ws>
2023-05-01 11:13:36 -0700

    styled follower and followings modal

--- react-app/src/components/Navigation/UserFollowing.js
+++ react-app/src/components/Navigation/UserFollowing.js
@@ -9,30 +9,25 @@ export default function UserFollowing({ userId }) {
     const followings = useSelector(state => state.session?.user?.followings)
     const session = useSelector(state => state.session)
 
-    const FollowingUser = ({ following, idx }) => {
+    const SingleFollowing = ({ following, idx }) => {
         const [followed, setFollowed] = useState(true);
         const handleFollowButton = (target_user_id) => {
             if (session?.user) {
                 setFollowed(!followed);
-                //console.log(following)
                 dispatch(followUnfollowUser(target_user_id, userId))
-            } else {
-                console.log("You need to belogged in to test that feature!")
             }
         };
-        return (
-            <div className="post-details post-details-user-menu">
-                <span>
-                    <img className="user-follow-image" src={following?.profile_picture} alt="following img" />
-                </span>
-                <span className="post-user">
-                    <span className="user-username">{following?.username}</span>
-                    <span className={`follow-user-button ${(following?.username === session?.user?.username) && "hidden"}`}>
-                        <div className="follow-button-container">
-                            <span className="follow-user-button" onClick={() => handleFollowButton(following.id)}>{followed ? "Unfollow" : "Follow"}</span>
-                        </div>
 
-                    </span>
+        return (
+            <div className="single-following">
+                <div className="single-following-user-info">
+                    <img className="user-follow-image" src={following?.profile_picture} alt="following img" />
+                    <span className="user-username">{following?.username}</span>
+                </div>
+                <span className={`follow-user-button ${(following?.username === session?.user?.username) && "hidden"}`}>
+                    <div className="follow-button-container">
+                        <span className="follow-user-button" onClick={() => handleFollowButton(following.id)}>{followed ? "Unfollow" : "Follow"}</span>
+                    </div>
                 </span>
             </div>
         )
@@ -40,17 +35,20 @@ export default function UserFollowing({ userId }) {
 
 
     return (
-        <>
-            <h2>
-                Following Users
+        <div id="following-modal">
+            <h2 id="following-modal-prompt">
+                {followings && followings.length} Following
             </h2>
-            {followings && followings.map((following, idx) => (
-                <div key={idx} >
-                    <FollowingUser following={following} idx={idx} />
-                </div>
-
-
-            ))}
-        </>
+            <div id="followings-container">
+                {(followings && followings.length) ? followings.map((following, idx) => (
+                    <SingleFollowing following={following} key={idx} />
+                ))
+                    :
+                    <div className="no-followings-message">
+                        Looks a little empty here...
+                    </div>
+                }
+            </div>
+        </div>
     )
 }
